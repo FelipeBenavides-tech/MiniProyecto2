425ed8731d95fdbfe3b5a5b27ec07d372cf92721
felip <felip@FelipeBenavides>
2025-03-28 00:44:55 -0500

    MiniProyecto

---
+++ src/main/java/com/mycompany/salondebelleza/SalonDeBelleza.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.salondebelleza;
+
+/**
+ *
+ * @author felip
+ */
+public class SalonDeBelleza {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
